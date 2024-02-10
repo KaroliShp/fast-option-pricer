e5b6f4d34efc3d98bfa5d5481eac38dcc7a3b5e8
Karolis <karolis.spukas@gmail.com>
2024-02-10 20:26:24 +0800

    Some testing with highway

--- fast_option_pricer/fast_math_helper.h
+++ fast_option_pricer/fast_math_helper.h
@@ -11,9 +11,14 @@ namespace fast_option_pricer {
 
 struct FastMathHelper
 {
-    [[nodiscard]] static inline double normal_cdf(double x)
+    explicit FastMathHelper()
+    {
+    }
+
+    [[nodiscard]] double normal_cdf(double x)
     {
         return 0.5;
+        // return 0.5 * std::erfc(-x / std::sqrt(2));
     }
 
     [[nodiscard]] static inline double d1(
--- tests/fast_math_helper_test.cpp
+++ tests/fast_math_helper_test.cpp
@@ -4,18 +4,43 @@
 
 #include "fast_math_helper.h"
 #include <gtest/gtest.h>
+#include <hwy/highway.h>
+#include <iostream>
+#include <vector>
+
+namespace hn = hwy::HWY_NAMESPACE;
 
 namespace fast_option_pricer {
 
 class FastMathHelperTest : public ::testing::Test
 {
+   public:
+    explicit FastMathHelperTest()
+    {
+    }
+
+    FastMathHelper fast_math_helper;
 };
 
 TEST_F(FastMathHelperTest, NormalCdf)
 {
-    auto res = fast_option_pricer::FastMathHelper::normal_cdf(0);
+    const hn::ScalableTag<float> d;
+    std::cout << "Lanes: " << hn::Lanes(d) << std::endl;
+    std::vector<float> inputs{0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0};
+    std::vector<float> output{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+    for (size_t i = 0; i < inputs.size(); i += hn::Lanes(d)) {
+        std::cout << i << std::endl;
+        hn::Vec128<float> mul = hn::Load(d, inputs.data() + i);
+        mul = hn::MulAdd(mul, mul, mul);
+        hn::Store(mul, d, output.data() + i);
+    }
+    for (int i = 0; i < output.size(); ++i) {
+        std::cout << i << ": " << output[i] << std::endl;
+    }
+    std::cout << "Lanes: " << hn::Lanes(d) << std::endl;
+    auto res = fast_math_helper.normal_cdf(0);
     EXPECT_DOUBLE_EQ(0.5, res);
     EXPECT_TRUE(true);
 }
 
-}
+}  // namespace fast_option_pricer
