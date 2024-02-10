257b418582cd8cf3e2dccb05f6f07df517c1095e
Karolis <karolis.spukas@gmail.com>
2024-02-11 01:50:30 +0800

    Add randomized tests

--- tests/black_scholes_test.cpp
+++ tests/black_scholes_test.cpp
@@ -5,6 +5,7 @@
 #include <benchmark/benchmark.h>
 #include <gtest/gtest.h>
 #include <hwy/highway.h>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include "common.h"
@@ -19,31 +20,62 @@ class BlackScholesTest : public ::testing::Test
 {
    public:
     using T = double;
+
+    BlackScholesTest()
+        : underlyings(num_options, 0),
+          strikes(num_options, 0),
+          risk_free_rates(num_options, 0),
+          volatilities(num_options, 0),
+          times_to_expiry(num_options, 0),
+          dividend_yields(num_options, 0)
+    {
+        for (size_t i = 0; i < num_options; ++i) {
+            underlyings[i] = rng(500.0);
+            strikes[i] = rng(500.0);
+            risk_free_rates[i] = rng(0.10);
+            volatilities[i] = rng(0.4);
+            times_to_expiry[i] = rng(251.0) / 252.0;
+            dividend_yields[i] = rng(0.10);
+        }
+    }
+
+    [[nodiscard]] T rng(float HI)
+    {
+        return static_cast<T>(std::rand()) / (static_cast<T>(RAND_MAX / HI));
+    }
+
+    size_t num_options{100000};
+    std::vector<T> underlyings;
+    std::vector<T> strikes;
+    std::vector<T> risk_free_rates;
+    std::vector<T> volatilities;
+    std::vector<T> times_to_expiry;
+    std::vector<T> dividend_yields;
 };
 
 TEST_F(BlackScholesTest, ComparePrice)
 {
     // Assign
-    std::vector<T> spot{110.0, 110.0, 110.0, 110.0};
+    /*std::vector<T> spot{110.0, 110.0, 110.0, 110.0};
     std::vector<T> strike{120.0, 120.0, 120.0, 120.0};
     std::vector<T> years_to_expiry(spot.size(), 25.0 / 252.0);
     std::vector<T> risk_free_rate(spot.size(), 0.02);
     std::vector<T> volatility{0.15, 0.16, 0.17, 0.18};
-    std::vector<T> dividend_yield{0.05, 0.05, 0.05, 0.05};
+    std::vector<T> dividend_yield{0.05, 0.05, 0.05, 0.05};*/
 
     // Act
     OptionPricing<T> fast_op_call(
-        spot, strike, risk_free_rate, volatility, years_to_expiry,
-        dividend_yield);
+        underlyings, strikes, risk_free_rates, volatilities, times_to_expiry,
+        dividend_yields);
     OptionPricing<T> fast_op_put(
-        spot, strike, risk_free_rate, volatility, years_to_expiry,
-        dividend_yield);
+        underlyings, strikes, risk_free_rates, volatilities, times_to_expiry,
+        dividend_yields);
     OptionPricing<T> naive_op_call(
-        spot, strike, risk_free_rate, volatility, years_to_expiry,
-        dividend_yield);
+        underlyings, strikes, risk_free_rates, volatilities, times_to_expiry,
+        dividend_yields);
     OptionPricing<T> naive_op_put(
-        spot, strike, risk_free_rate, volatility, years_to_expiry,
-        dividend_yield);
+        underlyings, strikes, risk_free_rates, volatilities, times_to_expiry,
+        dividend_yields);
 
     // Assert
     FastBlackScholes<T, hn::ScalableTag<T>>::price<true>(fast_op_call);
@@ -51,7 +83,7 @@ TEST_F(BlackScholesTest, ComparePrice)
     EXPECT_EQ(fast_op_call.prices.size(), naive_op_call.prices.size());
     EXPECT_EQ(fast_op_call.deltas.size(), naive_op_call.deltas.size());
     for (auto i = 0; i < fast_op_call.prices.size(); ++i) {
-        std::cout << "Elem: " << i << std::endl;
+        //std::cout << "Elem: " << i << std::endl;
         EXPECT_NEAR(fast_op_call.prices[i], naive_op_call.prices[i], 1e-5);
         EXPECT_NEAR(fast_op_call.deltas[i], naive_op_call.deltas[i], 1e-5);
     }
@@ -61,7 +93,7 @@ TEST_F(BlackScholesTest, ComparePrice)
     EXPECT_EQ(fast_op_put.prices.size(), naive_op_put.prices.size());
     EXPECT_EQ(fast_op_put.deltas.size(), naive_op_put.deltas.size());
     for (auto i = 0; i < fast_op_put.prices.size(); ++i) {
-        std::cout << "Elem: " << i << std::endl;
+        //std::cout << "Elem: " << i << std::endl;
         EXPECT_NEAR(fast_op_put.prices[i], naive_op_put.prices[i], 1e-5);
         EXPECT_NEAR(fast_op_put.deltas[i], naive_op_put.deltas[i], 1e-5);
     }
